6b83a596a1313c4daea45fc84992214bd87ec1d6
MaGrra <mrti.grra@gmail.com>
2023-03-10 19:02:55 +0200

    Optimize input function
    Takes numbers + operators

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const numbers = ['1','2','3','4','5','6','7','8','9','0','.']
+const operators = ['+','-','*','/'];
+const numButtons = document.querySelectorAll('.number');
+const operatorBtns = document.querySelectorAll('.operator');
+const screen = document.querySelector('.numberUI')
+const button = document.querySelectorAll('.button')
+
 function operator(fun, num1, num2) {
 
 }
@@ -14,16 +21,39 @@ function divide(num1,num2) {
     return num1 / num2;
 }
 function getInput() {
-    console.log(this.innerText);
+    screen.innerHTML += this.innerText;
 }
-const keys = ['1','2','3','4','5','6','7','8','9','0','/','*','-','+','Enter','.',',']
-const buttons = document.querySelectorAll('.button');
 
-function check (e) {
-    for (let i = 0; i < keys.length; i++) {
-        if (keys[i] == e.key) console.log(e.key);
+
+/*Gets input -keyboard - click*/
+window.addEventListener('keydown',checkInput);
+button.forEach(button => button.addEventListener('click', checkInput));
+
+function checkInput(e) {
+    if (this.innerHTML) {
+        for (let i = 0; i < numbers.length; i++) {
+            if (numbers[i] == this.innerHTML) {
+                 screen.innerHTML += numbers[i];
+                 break;
+            }
+         else if (operators[i] == this.innerHTML) {
+                console.log(this.innerHTML) ;
+                break;
+            }
+        }
+    } else if (!this.innerText) {
+        for (let i = 0; i < numbers.length; i++) {
+            if (numbers[i] == e.key) {
+                 screen.innerHTML += numbers[i];
+                 break;
+            }
+         else if (operators[i] == e.key) {
+                console.log(e.key) ;
+                break;
+            }
     }
+ }
 };
-window.addEventListener('keydown',check);
 
-buttons.forEach(button => button.addEventListener('click', getInput));
+
+
