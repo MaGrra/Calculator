8fa94b30ad0a50e0cae2c70f770e5b81bf4e1298
MaGrra <mrti.grra@gmail.com>
2023-03-10 16:40:20 +0200

    Add input functionality (console)
    Implement buttons and clicks with correct values

--- script.js
+++ script.js
@@ -13,4 +13,17 @@ function multiply(num1,num2) {
 function divide(num1,num2) {
     return num1 / num2;
 }
+function getInput() {
+    console.log(this.innerText);
+}
+const keys = ['1','2','3','4','5','6','7','8','9','0','/','*','-','+','Enter','.',',']
+const buttons = document.querySelectorAll('.button');
 
+function check (e) {
+    for (let i = 0; i < keys.length; i++) {
+        if (keys[i] == e.key) console.log(e.key);
+    }
+};
+window.addEventListener('keydown',check);
+
+buttons.forEach(button => button.addEventListener('click', getInput));
