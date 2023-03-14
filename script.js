64e903cf96451de04473ad87053f844fccfc1580
MaGrra <mrti.grra@gmail.com>
2023-03-14 14:54:08 +0200

    Input works, started making operator
    Adding function works with switch statement
    Input for numbers works
    Clear button works
    Numbers clear when writing new numbers (click only)

--- script.js
+++ script.js
@@ -4,12 +4,31 @@ const numButtons = document.querySelectorAll('.number');
 const operatorBtns = document.querySelectorAll('.operator');
 const screen = document.querySelector('.numberUI')
 const button = document.querySelectorAll('.button')
+const clear = document.querySelector('.clear');
 
-function operator(fun, num1, num2) {
-
+function operator(operation, num1, num2) {
+switch (operation)
+{
+    case '+':
+        console.log('iet plus');
+        add(num1,num2);
+        break;
+    case '-':
+        console.log('iet minus')
+        subtract(num1,num2);
+    case '*':
+        console.log('iet reiz');
+        multiply(num1,num2);
+    case '/':
+        console.log('iet dalit');
+        divide(num1, num2);
+    default: console.log(operation)
 }
-function add(num1, num2) {
-    return num1 + num2;
+}
+function add(worknum, num2) {
+    num1 = Number(worknum) + Number(num2);
+    screen.innerHTML = num1;
+    return num1;
 }
 function subtract(num1, num2) {
     return num1 - num2;
@@ -20,16 +39,45 @@ function multiply(num1,num2) {
 function divide(num1,num2) {
     return num1 / num2;
 }
-function getInput() {
-    screen.innerHTML += this.innerText;
-}
+
+let num1 = null;
+let num2 = null;
+let thing = false;
+let operation = '';
+
+function poga() {
+    if (!num1) {
+        num1 = screen.innerHTML;
+        thing = true;
+    } else {
+        num2 = screen.innerHTML;
+        console.log('num2', num2)
+        console.log('num1', num1)
+        thing = true;
+        operator(operation,num1,num2)
+    }
+};
 
 
 /*Gets input -keyboard - click*/
 window.addEventListener('keydown',checkInput);
 button.forEach(button => button.addEventListener('click', checkInput));
+clear.addEventListener('click', clearAll)
+function clearAll() {
+    num1 = null;
+    num2 = null;
+    screen.innerHTML = '';
+    thing = false;
+}
+function delScreen() {
+        screen.innerHTML = '';
+}
 
 function checkInput(e) {
+    if (thing == true && screen.innerHTML.length > 0) {
+        delScreen();
+        thing = false;
+    }
     if (this.innerHTML) {
         for (let i = 0; i < numbers.length; i++) {
             if (numbers[i] == this.innerHTML) {
@@ -37,7 +85,9 @@ function checkInput(e) {
                  break;
             }
          else if (operators[i] == this.innerHTML) {
-                console.log(this.innerHTML) ;
+                operation = this.innerHTML;
+                operation = this.innerHTML;
+                poga();
                 break;
             }
         }
@@ -54,6 +104,5 @@ function checkInput(e) {
     }
  }
 };
-
 
 
