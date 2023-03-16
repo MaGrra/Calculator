61eecbec0c9901488678452a5892867b43f3e274
MaGrra <mrti.grra@gmail.com>
2023-03-16 19:33:49 +0200

    Update design + number keyboard animations
    Operations not animating due to innerHtml values

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ const equals =['=','Enter'];
 const numButtons = document.querySelectorAll('.number');
 const operatorBtns = document.querySelectorAll('.operator');
 const screen = document.querySelector('.numberUI')
-const button = document.querySelectorAll('.button')
+const buttons = document.querySelectorAll('.button')
 const clearAllMemory = document.querySelector('.clearAll');
 const clear = document.querySelector('.clear');
 const plusMinusBtn = document.querySelector('.plusMinus');
 const percentBtn = document.querySelector('.percent');
+buttons.forEach(button => button.classList.add(`m${button.innerHTML}m`) )
 
 let num1 = null;
 let num2 = null;
@@ -48,7 +49,6 @@ switch (operation)
     case 'Enter':
         operation = previousOperator;
         operator(previousOperator,num1,num2);
-    default: console.log(operation)
 }
 
 }
@@ -66,7 +66,6 @@ function multiply(worknum,worknum2) {
     num1 = Number(worknum) * Number(worknum2);
     screen.innerHTML = Math.round(num1 * 100) / 100;
     num1 = Math.round(num1 * 100) / 100;
-
 }
 function divide(worknum,num2) {
     num1 = Number(worknum) / Number(num2);
@@ -83,7 +82,7 @@ function clearAll() {
     previousOperator = '';
     operation = '';
 }
-function delScreen() {
+function clearScreen() {
         screen.innerHTML = '';
         
 }
@@ -99,13 +98,18 @@ function changeSign() {
         }
     }
 };
-function percentFun() {
-    screen.innerHTML = screen.innerHTML / 100;
-            num1 = Number(num1);
+function percentageFunction() {
+    if (screen.innerHTML == 0) {
+    screen.innerHTML = 0;
+    return;
+    } else {
+        screen.innerHTML = screen.innerHTML / 100;
+    num1 = Number(num1);
+    }
 }
 
 /* Evaluates the rules for operator before passing them along to the operator function */
-function poga() {
+function DecideOperation() {
     if (resultDisplayed == true ) {
         screen.innerHTML = num1;
         resultDisplayed = false;
@@ -148,25 +152,31 @@ function sum() {
     operator(operation,num1,num2);
     resultDisplayed = true;
 };
-
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return; 
+    console.log(e.propertyName);
+    this.classList.remove('btnClicked')
+}
 
 /*Gets input -keyboard - click*/
 window.addEventListener('keydown',checkInput);
-button.forEach(button => button.addEventListener('click', checkInput));
+buttons.forEach(button => button.addEventListener('click', checkInput));
 clearAllMemory.addEventListener('click', clearAll)
-clear.addEventListener('click', delScreen);
+clear.addEventListener('click', clearScreen);
 plusMinusBtn.addEventListener('click', changeSign);
-percentBtn.addEventListener('click', percentFun)
+percentBtn.addEventListener('click', percentageFunction)
+
+
 
 
 function checkInput(e) {
-
     if (startNewNumber == true) {
         tempNum = num2;
-        delScreen();
+        clearScreen();
         startNewNumber = false;
     }
     if (this.innerHTML) {
+        this.classList.add('btnClicked');
         for (let i = 0; i < numbers.length; i++) {
             if (numbers[i] == this.innerHTML) {
                 if (screen.innerHTML.length > 9) return;
@@ -175,7 +185,7 @@ function checkInput(e) {
                  break;
             } else if (operators[i] == this.innerHTML) {
                 operation = this.innerHTML;
-                poga();
+                DecideOperation();
                 break;
             } else if (equals[i] == this.innerHTML) {
                 operation = this.innerHTML
@@ -185,15 +195,21 @@ function checkInput(e) {
         
         }
     } else if (!this.innerText) {
+        console.log(e.key);
+        
         for (let i = 0; i < numbers.length; i++) {
+            if (e.key == 'Escape' || e.key == 'Delete') clearAll();
+            if (e.key == 'Backspace') clearScreen();
             if (numbers[i] == e.key) {
                 if (screen.innerHTML.length > 9) return;
                 if (screen.innerHTML.includes('.') && e.key == '.') return;
                  screen.innerHTML += numbers[i];
+                 const key = document.querySelector(`.m${e.key}m`)
+                 key.classList.add('btnClicked');
                  break;
             } else if (operators[i] == e.key) {
                 operation = e.key;
-                poga();
+                DecideOperation();
                 break;
             } else if (equals[i] == e.key) {
                 operation = e.key
@@ -205,12 +221,12 @@ function checkInput(e) {
     if (operation !== '=' && operation !== 'Enter') {
         previousOperator = operation;
     }
-    if (previousOperator == '/' && num2 == 0) 
+    if ((previousOperator == '/' ||  previousOperator == '÷') && num2 == 0) 
     {
         clearAll()
-        return screen.innerHTML = "Don't do this!";
-        
+        screen.innerHTML = "Don't do that!";
     }
+ 
 };
 
-
+buttons.forEach(button => button.addEventListener('transitionend', removeTransition));
