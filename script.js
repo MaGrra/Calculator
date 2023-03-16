0a675291f17cbc4ff4579f4af57ce025cfae92b9
MaGrra <mrti.grra@gmail.com>
2023-03-16 15:15:13 +0200

    Complete mathemathical functionality
    Basic work design
    All inputs work with functions
    No known issues remaining

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const numbers = ['1','2','3','4','5','6','7','8','9','0','.']
-const operators = ['+','-','*','/',];
+const operators = ['+','-','*','/','÷'];
 const equals =['=','Enter'];
 const numButtons = document.querySelectorAll('.number');
 const operatorBtns = document.querySelectorAll('.operator');
@@ -7,16 +7,19 @@ const screen = document.querySelector('.numberUI')
 const button = document.querySelectorAll('.button')
 const clearAllMemory = document.querySelector('.clearAll');
 const clear = document.querySelector('.clear');
+const plusMinusBtn = document.querySelector('.plusMinus');
+const percentBtn = document.querySelector('.percent');
 
 let num1 = null;
 let num2 = null;
-let num3 = null;
-let idkYet = false;
+let tempNum = null;
+let resultDisplayed = false;
 let startNewNumber = true;
 let operation = '';
-let huj = '';
+let previousOperator = '';
 screen.innerHTML = '0'
 
+/* Works the actual calculation */
 function operator(operation, num1, num2) {
 switch (operation)
 {
@@ -27,61 +30,85 @@ switch (operation)
         subtract(num1,num2);
         break;
     case '*':
+        if (!num2) {
+            screen.innerHTML = num1;
+            return;
+        }
         multiply(num1,num2);
         break;
     case '/':
+    case '÷':
+        if (!num2) {
+            screen.innerHTML = num1;
+            return;
+        }
         divide(num1, num2);
         break;
     case '=':
     case 'Enter':
-        operation = huj;
-        operator(huj,num1,num2);
+        operation = previousOperator;
+        operator(previousOperator,num1,num2);
     default: console.log(operation)
 }
+
 }
 function add(worknum, worknum2) {
     num1 = Number(worknum) + Number(worknum2);
-    screen.innerHTML = num1;
-    return num1;
+    screen.innerHTML = Math.round(num1 * 100) / 100;
+    num1 = Math.round(num1 * 100) / 100;
 }
 function subtract(worknum, num2) {
     num1 = Number(worknum) - Number(num2);
-    screen.innerHTML = num1;
-    return num1;
+    screen.innerHTML = Math.round(num1 * 100) / 100;
+    num1 = Math.round(num1 * 100) / 100;
 }
 function multiply(worknum,worknum2) {
     num1 = Number(worknum) * Number(worknum2);
-    screen.innerHTML = num1;
-    return num1, num2;
+    screen.innerHTML = Math.round(num1 * 100) / 100;
+    num1 = Math.round(num1 * 100) / 100;
+
 }
 function divide(worknum,num2) {
     num1 = Number(worknum) / Number(num2);
-    screen.innerHTML = num1;
-    return num1;
+    screen.innerHTML = Math.round(num1 * 100) / 100;
+    num1 = Math.round(num1 * 100) / 100;
 }
 function clearAll() {
     num1 = null;
     num2 = null;
-    num3 = null;
+    tempNum = null;
     screen.innerHTML = '0';
     startNewNumber = true;
-    idkYet = false;
-    huj = '';
+    resultDisplayed = false;
+    previousOperator = '';
     operation = '';
 }
 function delScreen() {
         screen.innerHTML = '';
         
 }
+function changeSign() {
+    if (screen.innerHTML > 0) {
+        if (screen.innerHTML.includes('-')) return;
+            screen.innerHTML =  '-' + screen.innerHTML;
+            num1 = '-' + Number(num1);
+    } else if (screen.innerHTML < 0) {
+        if (screen.innerHTML.charAt(0) === '-') {
+            screen.innerHTML = screen.innerHTML.substring(1);
+            num1 = screen.innerHTML;
+        }
+    }
+};
+function percentFun() {
+    screen.innerHTML = screen.innerHTML / 100;
+            num1 = Number(num1);
+}
 
-
-
+/* Evaluates the rules for operator before passing them along to the operator function */
 function poga() {
-    console.log('pog op', operation)
-    console.log('pog huj',huj)
-    if (idkYet == true ) {
+    if (resultDisplayed == true ) {
         screen.innerHTML = num1;
-        idkYet = false;
+        resultDisplayed = false;
         startNewNumber = true;
         num2 = null;
         return;
@@ -90,39 +117,36 @@ function poga() {
     if (!num1) {
         num1 = screen.innerHTML;
         startNewNumber = true;
-    } else if (!num2 && !huj) {
+    } else if (!num2 && !previousOperator) {
         
         num2 = screen.innerHTML;
-        num3 = num2
+        tempNum = num2
         startNewNumber = true;
         operator(operation,num1,num2)
          
-    } else if (operation != huj) {
+    } else if (operation != previousOperator) {
             startNewNumber = true;
             num2 = screen.innerHTML;
-            num3 = num2;
-            operator(huj,num1,num2)
+            tempNum = num2;
+            operator(previousOperator,num1,num2)
             return;
             
     } else {
         num2 = screen.innerHTML;
-        console.log('suka')
         startNewNumber = true;
         operator(operation,num1,num2);
-        num2 = num3;
+        num2 = tempNum;
         
     }
-    console.error(operation)
 };
+/* Function is ewoked when input gets '=' evaluates logic before starting the operator */
 function sum() {
-    console.log('sum op', operation)
-    console.log('sum huj',huj)
-    if (idkYet == false) {
+    if (resultDisplayed == false) {
         num2 = screen.innerHTML;
     } 
   
     operator(operation,num1,num2);
-    idkYet = true;
+    resultDisplayed = true;
 };
 
 
@@ -131,14 +155,14 @@ window.addEventListener('keydown',checkInput);
 button.forEach(button => button.addEventListener('click', checkInput));
 clearAllMemory.addEventListener('click', clearAll)
 clear.addEventListener('click', delScreen);
+plusMinusBtn.addEventListener('click', changeSign);
+percentBtn.addEventListener('click', percentFun)
 
 
 function checkInput(e) {
-    console.log('clear',startNewNumber);
-    console.log('num',idkYet);
 
     if (startNewNumber == true) {
-        num3 = num2;
+        tempNum = num2;
         delScreen();
         startNewNumber = false;
     }
@@ -146,6 +170,7 @@ function checkInput(e) {
         for (let i = 0; i < numbers.length; i++) {
             if (numbers[i] == this.innerHTML) {
                 if (screen.innerHTML.length > 9) return;
+                if (screen.innerHTML.includes('.') && this.innerHTML == '.') return;
                  screen.innerHTML += numbers[i];
                  break;
             } else if (operators[i] == this.innerHTML) {
@@ -163,6 +188,7 @@ function checkInput(e) {
         for (let i = 0; i < numbers.length; i++) {
             if (numbers[i] == e.key) {
                 if (screen.innerHTML.length > 9) return;
+                if (screen.innerHTML.includes('.') && e.key == '.') return;
                  screen.innerHTML += numbers[i];
                  break;
             } else if (operators[i] == e.key) {
@@ -177,9 +203,14 @@ function checkInput(e) {
         }
     }
     if (operation !== '=' && operation !== 'Enter') {
-        huj = operation;
+        previousOperator = operation;
     }
-    console.warn(screen.innerHTML);
+    if (previousOperator == '/' && num2 == 0) 
+    {
+        clearAll()
+        return screen.innerHTML = "Don't do this!";
+        
+    }
 };
 
 
